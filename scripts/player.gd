5dc6a2ba5243a9e3e2f30456ddb65cf56fd861a2
peingoin <austincheng2009@gmail.com>
2025-09-27 18:49:23 -0400

    hi

---
+++ scripts/player.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+const speed = 400.0
+const accel = 2.0
+
+var input: Vector2
+
+func get_input():
+	input.x = Input.get_action_strength("right") - Input.get_action_strength("left")
+	input.y = Input.get_action_strength("down") - Input.get_action_strength("up")
+	return input.normalized()
+
+
+func _process(delta):
+	var player_input = get_input()
+	
+	velocity = lerp(velocity, player_input * speed, delta * accel)
+	
+	move_and_slide()
